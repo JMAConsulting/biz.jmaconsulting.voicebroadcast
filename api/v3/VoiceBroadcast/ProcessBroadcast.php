2743a5d46d3c52840f9cacb723a9b96c285ddae1
Edsel <edsel.lopez@jmaconsulting.biz>
2015-04-29 16:12:05 +0530

    VB-9 Modification to API and changes in install script

---
+++ api/v3/VoiceBroadcast/ProcessBroadcast.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * VoiceBroadcast.ProcessBroadcast API
+ *
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_voicebroadcast_processbroadcast($params) {
+  exit;
+
+  if (!CRM_VoiceBroadcast_BAO_VoiceBroadcast::processQueue()) {
+    return civicrm_api3_create_error(ts('Process VoiceBroadcast failed'));
+  }
+  else {
+    return civicrm_api3_create_success(ts('VoiceBroadcasts were processed successfully!'));
+  }
+}
--- sql/voicebroadcast_uninstall.sql
+++ sql/voicebroadcast_uninstall.sql
@@ -1,17 +1,20 @@
--- /*******************************************************
--- *
--- * voice Extension
--- *
--- * Eftakhairul Islam <eftakhairul@gmail.com>
--- *******************************************************/
+DROP TABLE IF EXISTS `civicrm_voicebroadcast`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_calls`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_event_delivered`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_event_queue`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_group`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_job`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_lookup`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_mapping`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_plivo`;
+DROP TABLE IF EXISTS `civicrm_voicebroadcast_recipients`;
 
-DROP TABLE IF EXISTS `civicrm_voice_braodcast_group`;
-DROP TABLE IF EXISTS `civicrm_voice_broadcast`;
-DROP TABLE IF EXISTS `civicrm_voice_broadcast_call`;
-DROP TABLE IF EXISTS `civicrm_voice_broadcast_job`;
-DROP TABLE IF EXISTS `civicrm_voice_broadcast_recipients`;
-DROP TABLE IF EXISTS `civicrm_voice_broadcast_response`;
-DROP TABLE IF EXISTS `civicrm_voice_broadcast_spool`;
+DELETE FROM TABLE `civicrm_navigation` WHERE `name` = 'Voice Broadcasts';
+DELETE FROM TABLE `civicrm_navigation` WHERE `name` = 'VoiceBroadcast Plivo Integration';
+DELETE FROM TABLE `civicrm_navigation` WHERE `name` = 'New Voice Broadcast';
+DELETE FROM TABLE `civicrm_navigation` WHERE `name` = 'Scheduled and Sent Broadcasts';
 
--- Delete the row from civicrm cron job
-DELETE FROM `civicrm`.`civicrm_job` WHERE `civicrm_job`.`api_action` = 'processvoicebroadcast' AND `civicrm_job`.`api_entity` = 'job';
+DELETE FROM TABLE `civicrm_option_group` WHERE `name` = 'file_type';
+DELETE FROM TABLE `civicrm_option_value` WHERE `name` = 'Voice Broadcast';
+DELETE FROM TABLE `civicrm_option_value` WHERE `name` = 'wav';
+DELETE FROM TABLE `civicrm_option_value` WHERE `name` = 'mp3';
